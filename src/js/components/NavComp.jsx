53bfef3385fda54711872aea86c6183021bfe751
Madison G Verger <madisonverger613@gmail.com>
2018-07-27 19:51:54 +0000

    one last thing

--- src/js/components/NavComp.jsx
+++ src/js/components/NavComp.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
-
+import SessionStore from '../stores/SessionStore.js';
 class NavComponent extends React.Component{
+    similarMedia() {
+        const storeState = SessionStore.getState();
+        let user = storeState.login.username;
+        let url = 'https://mediamatchserver-madechai.c9users.io/getmediashort/'+user;
+        fetch(url, {
+            method: 'GET', // or 'PUT'
+            headers:{
+            'Content-Type': 'application/json'
+        }
+        }).then(res => res.json())
+        .catch(error => console.error('Error:', error))
+        .then(response => this.printobj(response));
+    }
+    printobj(objects) {
+        console.log(objects);
+        let str = "";
+        for (let i=0; i<objects.length; i++) {
+            str = str+objects[i].creator_or_title + ", type: " + objects[i].content_type + "\n";
+        }
+        alert(str);
+    }
     render(){
         const padding = {
             backgroundColor: "#ff8584"
         };   
+        
         return (
             <div className="container-fluid" style={padding}>
                 <nav className="navbar navbar-expand-lg bgg textp">
@@ -20,11 +42,11 @@ class NavComponent extends React.Component{
                             <li className="nav-item active">
                                 <Link to="/profile" className="nav-link textp">Profile<span className="sr-only">(current)</span></Link>
                             </li>
-                            <li className="nav-item active">
-                                <Link to="/addwidget" className="nav-link textp">Add Widget<span className="sr-only">(current)</span></Link>
-                            </li>
                             <li className="nav-item">
                                 <Link to="/friend" className="nav-link textp">Friend Selector</Link>
+                            </li>
+                            <li className="nav-item active">
+                                <button height="15px" onClick= {() => this.similarMedia()}>Get Similar Media</button>
                             </li>
                         </ul>
                         <form className="form-inline my-2 my-lg-0">
